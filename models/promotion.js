a222fed50f993b6f7b25d37f38e5b58cedbcc189
Eloy Lang <thisiseloy@gmail.com>
2021-06-26 10:39:23 -0700

    Finished Workshop 2: Promises for Promotion and Partner Routers & Partner and Promotion Schemas

---
+++ models/promotion.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+const Schema = mongoose.Schema;
+
+const promotionSchema = new Schema ({
+
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String, //will be a path to the image
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    cost: {
+        type: Currency, //$$$
+        required: true,
+        min: 0 //-$$$
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Promotion = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotion;
